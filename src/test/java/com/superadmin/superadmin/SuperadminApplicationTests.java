498d09500fc61b950ecbf722e7a5847fe4c23972
Backend <successitpune@gmail.com>
2024-05-29 16:53:34 +0530

    initial commit for these project

---
+++ src/test/java/com/superadmin/superadmin/SuperadminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.superadmin.superadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperadminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
